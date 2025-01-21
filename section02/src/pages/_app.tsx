c31080f522b4a55992fc40549306f2506bc859b9
glory <click_syk@naver.com>
2025-01-21 22:49:32 +0900

    prefetch

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -10,12 +11,20 @@ export default function App({ Component, pageProps }: AppProps) {
     router.push('/test');
   };
 
+  // programtic하게 라우팅 되는 페이지를 프리페치하는 방법
+  useEffect(() => {
+    router.prefetch('/test');
+  }, []);
+
   return (
     <>
       <header>
         <Link href="/">index</Link>
         &nbsp;
-        <Link href="/search">search</Link>
+        {/* 프리페치 해제 */}
+        <Link href="/search" prefetch={false}>
+          search
+        </Link>
         &nbsp;
         <Link href="/book/1">book/1</Link>
         <div>
