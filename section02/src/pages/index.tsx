8d0ac7a136d841465f46631fd8f08815591003bc
83436 <glory@83436.local>
2025-01-31 20:54:32 +0900

    UI 구현하기

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,14 +1,25 @@
-// CSS module
+import BookItem from '@/components/book-item';
 import SearchableLayout from '@/components/searchable-layout';
+import books from '@/mock/books.json';
 import { ReactNode } from 'react';
 import style from './index.module.css';
 
 export default function Home() {
   return (
-    <>
-      <h1 className={style.h1}>인덱스</h1>
-      <h2 className={style.h2}>H2</h2>
-    </>
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
   );
 }
 
