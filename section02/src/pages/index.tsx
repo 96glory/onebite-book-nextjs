a0f4b12c665c59c85668f53ac9b18f8092c33bda
glory <click_syk@naver.com>
2025-02-02 14:53:07 +0900

    SSR

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,21 +1,39 @@
 import BookItem from '@/components/book-item';
 import SearchableLayout from '@/components/searchable-layout';
-import books from '@/mock/books.json';
+import fetchBooks from '@/lib/fetch-books';
+import fetchRandomBooks from '@/lib/fetch-random-books';
+import { InferGetServerSidePropsType } from 'next';
 import { ReactNode } from 'react';
 import style from './index.module.css';
 
-export default function Home() {
+// 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
+// node 환경 (서버) 에서만 실행되기 때문에 브라우저에서는 실행되지 않음
+export const getServerSideProps = async () => {
+  const [allBooks, recommandBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
+
+  return {
+    props: { allBooks, recommandBooks },
+  };
+};
+
+export default function Home({
+  allBooks,
+  recommandBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recommandBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
