ca6be6a900639f0e492b2c52f4a2b67a52ca2d05
glory <click_syk@naver.com>
2025-02-02 15:58:51 +0900

    SSG 2

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -2,13 +2,27 @@ import BookItem from '@/components/book-item';
 import SearchableLayout from '@/components/searchable-layout';
 import fetchBooks from '@/lib/fetch-books';
 import fetchRandomBooks from '@/lib/fetch-random-books';
-import { InferGetServerSidePropsType } from 'next';
+import { InferGetStaticPropsType } from 'next';
 import { ReactNode } from 'react';
 import style from './index.module.css';
 
 // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
 // node 환경 (서버) 에서만 실행되기 때문에 브라우저에서는 실행되지 않음
-export const getServerSideProps = async () => {
+// export const getServerSideProps = async () => {
+//   const [allBooks, recommandBooks] = await Promise.all([
+//     fetchBooks(),
+//     fetchRandomBooks(),
+//   ]);
+
+//   return {
+//     props: { allBooks, recommandBooks },
+//   };
+// };
+
+// SSG 방식
+export const getStaticProps = async () => {
+  console.log('getStaticProps');
+
   const [allBooks, recommandBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
@@ -22,7 +36,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   recommandBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
