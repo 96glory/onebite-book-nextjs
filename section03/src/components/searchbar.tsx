60bd5e522898fa45edd81404896b647ef092d592
glory <click_syk@naver.com>
2025-02-16 17:44:01 +0900

    네비게이팅

--- section03/src/components/searchbar.tsx
+++ section03/src/components/searchbar.tsx
@@ -1,18 +1,25 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { ChangeEvent, useState } from 'react';
 
 export default function Searchbar() {
+  const router = useRouter();
+
   const [search, setSearch] = useState('');
 
   const onChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
+  const onSubmit = () => {
+    router.push(`/search?q=${search}`);
+  };
+
   return (
     <div>
       <input value={search} onChange={onChangeSearch} />
-      <button>검색</button>
+      <button onClick={onSubmit}>검색</button>
     </div>
   );
 }
