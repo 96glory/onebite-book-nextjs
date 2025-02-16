d4eb99123d680ad69c9a5399207cdc9236860815
glory <click_syk@naver.com>
2025-02-16 22:08:10 +0900

    리퀘스트 메모이제이션

--- section04/src/app/layout.tsx
+++ section04/src/app/layout.tsx
@@ -1,6 +1,27 @@
+import { BookData } from '@/types';
 import Link from 'next/link';
 import './globals.css';
 import style from './layout.module.css';
+
+async function Footer() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+  );
+
+  if (!response.ok) {
+    return <footer>제작 @glory</footer>;
+  }
+
+  const books: BookData[] = await response.json();
+  const bookCount = books.length;
+
+  return (
+    <footer>
+      <div>제작 @glory</div>
+      <div>{bookCount}개의 도서가 등록되어 있습니다.</div>
+    </footer>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -15,7 +36,7 @@ export default function RootLayout({
             <Link href="/">📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @glory</footer>
+          <Footer />
         </div>
       </body>
     </html>
