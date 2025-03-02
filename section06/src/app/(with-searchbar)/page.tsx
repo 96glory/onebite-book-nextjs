d7ebeefde93c2f10d4fe442c5618b089ce9baf32
glory <click_syk@naver.com>
2025-03-02 14:57:15 +0900

    컴포넌트 스트리밍 적용하기

--- section06/src/app/(with-searchbar)/page.tsx
+++ section06/src/app/(with-searchbar)/page.tsx
@@ -1,5 +1,7 @@
 import BookItem from '@/components/book-item';
 import { BookData } from '@/types';
+import { delay } from '@/util/delay';
+import { Suspense } from 'react';
 import style from './page.module.css';
 
 /**
@@ -9,9 +11,11 @@ import style from './page.module.css';
  * 3) force-static : 페이지를 강제로 static 페이지로 설정
  * 4) error : 페이지를 강제로 static 페이지로 설정, static으로 설정하면 안되는 이유가 있을 때 build error
  */
-export const dynamic = 'auto';
+export const dynamic = 'force-dynamic';
 
 async function AllBooks() {
+  await delay(1500);
+
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
     { cache: 'force-cache' },
@@ -33,6 +37,8 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
+  await delay(3000);
+
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
     { next: { revalidate: 3 } },
@@ -58,11 +64,15 @@ export default function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecoBooks />
+        <Suspense fallback={<div>도서를 불러오는 중입니다...</div>}>
+          <RecoBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <AllBooks />
+        <Suspense fallback={<div>도서를 불러오는 중입니다...</div>}>
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   );
